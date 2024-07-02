a207bdb4689a9a18d90836c3735eb126b05a5e2b
Anderson Cusma <andersoncusma20@gmail.com>
2024-07-02 12:52:06 -0500

    ApplicationDTO

--- api-pet/src/main/java/com/pet/api_pet/dto/AdoptionDTO.java
+++ api-pet/src/main/java/com/pet/api_pet/dto/AdoptionDTO.java
@@ -1,4 +1,15 @@
 package com.pet.api_pet.dto;
 
+import com.pet.api_pet.model.Application;
+
+import java.time.Instant;
+import java.util.UUID;
+
 public class AdoptionDTO {
+
+    private UUID adoptionId;
+
+    private Instant adoptionDate;
+
+    private ApplicationDTO application;
 }
