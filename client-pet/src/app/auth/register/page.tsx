8d65972eca48b4db8659554d3e8a0ba92a5e6a3c
Brian Tarqui <tarquibrian@gmail.com>
2024-07-04 14:26:56 -0400

    UPDATE: actualizar la diseño auth 🎨

--- client-pet/src/app/auth/register/page.tsx
+++ client-pet/src/app/auth/register/page.tsx
@@ -1,7 +1,66 @@
 import React from "react";
+import logo from "../../../assets/logo-horizontal.png";
+import Image from "next/image";
+import { Button, Input } from "@components";
+import Link from "next/link";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="registerPage">
+      <div className="registerPage__picture">
+        <Image src={logo} alt="header logo" />
+      </div>
+      <div className="registerPage__wrapper">
+        <h3>Registro de Usuario</h3>
+        <div className="registerPage__wrapper--form">
+          <Input name="name" label="Nombre" placeholder="Nombre" type="text" />
+          <Input
+            name="lastname"
+            label="Apellidos"
+            placeholder="Apellido"
+            type="text"
+          />
+          <Input
+            name="email"
+            label="Correo"
+            placeholder="Ingresu su correo"
+            type="email"
+          />
+          <Input
+            name="phone"
+            label="Telefono"
+            placeholder="Némero de telefono"
+            type="text"
+          />
+          <Input
+            name="password"
+            label="Contraseña"
+            placeholder="Ingrese una contraseña"
+            type="password"
+          />
+          <Input
+            name="confirmPassword"
+            label="Confirmar Contraseña"
+            placeholder="Confirme la contraseña"
+            type="password"
+          />
+        </div>
+        <div className="registerPage__wrapper--options">
+          <label className="options-terms">
+            <input type="checkbox" checked />
+            <span className="label-text">
+              Acepto los términos y condiciones
+            </span>
+          </label>
+          <Button color="primary">Crear Cuena</Button>
+          <div className="options-link">
+            <span>Ya tienes cuenta?</span>
+            <Link href={"/auth/login"}>Iniciar Sesión</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
--- client-pet/src/app/page.tsx
+++ client-pet/src/app/page.tsx
@@ -1,12 +1,18 @@
+import { Navbar, Footer } from "@components";
+
 export default function Home() {
   return (
-    <main id="main-content">
-      <section className="main-content__hero">Hero Section</section>
-      <section className="main-content__features">Features Section</section>
-      <section className="main-content__services">Services Section</section>
-      <section className="main-content__info">Info Section</section>
-      <section className="main-content__reviews">Reviews Section</section>
-      <section className="main-content__faq">FAQs Section</section>
-    </main>
+    <>
+      <Navbar />
+      <main id="main-content">
+        <section className="main-content__hero">Hero Section</section>
+        <section className="main-content__features">Features Section</section>
+        <section className="main-content__services">Services Section</section>
+        <section className="main-content__info">Info Section</section>
+        <section className="main-content__reviews">Reviews Section</section>
+        <section className="main-content__faq">FAQs Section</section>
+      </main>
+      <Footer />
+    </>
   );
 }
