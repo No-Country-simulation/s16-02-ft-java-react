c0802ec1a6eb0925e6ec0bf90c45964b1e5aa942
Brian Tarqui <tarquibrian@gmail.com>
2024-07-05 18:21:32 -0400

    ADD: desarrollar la seccion hero ➕

--- client-pet/src/components/index.ts
+++ client-pet/src/components/index.ts
@@ -1,3 +1,6 @@
+export { default as Hero } from "./sections/Hero";
+export { default as About } from "./sections/About";
+
 export { default as Input } from "./common/Input";
 export { default as Button } from "./common/Button";
 
---
+++ client-pet/src/components/sections/Hero.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import img1 from "../../assets/carousel1.png";
+import img2 from "../../assets/carousel2.png";
+import img3 from "../../assets/carousel3.png";
+import img4 from "../../assets/carousel4.png";
+import Image from "next/image";
+import { Button } from "@components";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__header">
+        <h1>
+          Adoptar una mascota es abrir tu corazón a un nuevo miembro de la
+          familia, trayendo amor y un hogar lleno de felicidad.
+        </h1>
+        <Button color="primary">Adopta una mascota</Button>
+      </div>
+      <div className="hero__carousel">
+        <div className="hero__carousel--wrapper">
+          <div className="wrapper-row">
+            <div className="picture-wrapper">
+              <Image src={img1} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img2} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img3} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img4} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img1} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img2} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img3} alt="carousel hero img1" />
+            </div>
+            <div className="picture-wrapper">
+              <Image src={img4} alt="carousel hero img1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
