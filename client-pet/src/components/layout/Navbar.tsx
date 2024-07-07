6759173682a31159f09363cf3f604f45ef63c4ac
Brian Tarqui <tarquibrian@gmail.com>
2024-07-07 00:48:03 -0400

    ADD: desarrollar la seccion hero

--- client-pet/src/components/layout/Navbar.tsx
+++ client-pet/src/components/layout/Navbar.tsx
@@ -1,18 +1,32 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import logo from "../../assets/logo-horizontal.png";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className="main-navbar">
+      <Link href={"/"} className="main-navbar__logo">
+        <Image src={logo} alt="header logo" />
+      </Link>
       <ul className="main-navbar__menu">
         <li>
-          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>Nostros</Link>
         </li>
         <li>
           <Link href={"/adopcion"}>Adopcion</Link>
         </li>
         <li>
           <Link href={"/cuidado"}>Cuidado</Link>
+        </li>
+        <li>
+          <Link href={"/rescate"}>Rescate</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/donar"}>Donar</Link>
         </li>
       </ul>
       <div className="main-navbar__auth">
