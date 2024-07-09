bc9c2a3240ef47ad11811c342ba8684a96bf95e0
Brian Tarqui <tarquibrian@gmail.com>
2024-07-08 21:41:44 -0400

    ADD: agregar la seccion about➕

--- client-pet/src/components/common/Button.tsx
+++ client-pet/src/components/common/Button.tsx
@@ -3,10 +3,15 @@ import React from "react";
 interface ButtonProps {
   children: React.ReactNode;
   color: "primary" | "secondary";
+  className?: string;
 }
 
-const Button = ({ children, color }: ButtonProps) => {
-  return <button className={`button button__${color}`}>{children}</button>;
+const Button = ({ children, color, className }: ButtonProps) => {
+  return (
+    <button className={`button button__${color} ${className}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
