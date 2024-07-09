96624d00aada497bee67b3265a5f01280abbe35e
Brian Tarqui <tarquibrian@gmail.com>
2024-07-08 23:20:17 -0400

    ADD: agregar la seccion adopcion de inicio➕

--- client-pet/src/components/sections/AdoptionPets.tsx
+++ client-pet/src/components/sections/AdoptionPets.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import img from "../../assets/carousel3.png";
+import Image from "next/image";
 
 const AdoptionPets = () => {
-  return <div>AdoptionPets</div>;
+  return (
+    <section className="adoptionPets">
+      <header className="adoptionPets__header">
+        <h2>Algunos de nuestros amigos que buscan un hogar</h2>
+      </header>
+      <div className="adoptionPets__gallery">
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+        <div className="adoptionPets__gallery--card">
+          <Image src={img} alt="adoption pet gallery item" />
+          <div className="card-content">
+            <label>Name Pet</label>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AdoptionPets;
