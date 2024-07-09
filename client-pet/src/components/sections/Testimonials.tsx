59ec1d3a35318823e65d72092b79ac3e32b59ee7
Brian Tarqui <tarquibrian@gmail.com>
2024-07-09 10:55:23 -0400

    ADD: agregar testimonials seccion➕

--- client-pet/src/components/sections/Testimonials.tsx
+++ client-pet/src/components/sections/Testimonials.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section className="testimonials">
+      <header className="testimonials__header">
+        <h2>Historias de amor</h2>
+      </header>
+      <header className="testimonials__carousel">Testimonials</header>
+    </section>
+  );
 };
 
 export default Testimonials;
