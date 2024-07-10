1d8e39a45d1ba1cc63be438c45f516289fa93f24
Brian Tarqui <tarquibrian@gmail.com>
2024-07-10 00:28:47 -0400

    ADD➕: agregar el layout de testimonials

--- client-pet/src/components/sections/Testimonials.tsx
+++ client-pet/src/components/sections/Testimonials.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Card } from "@components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay } from "swiper";
+import { Autoplay, Navigation } from "swiper/modules";
 
 import "swiper/css/bundle";
 import "swiper/css";
@@ -10,29 +10,6 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const Testimonials = () => {
-  const swiperRef = useRef<HTMLDivElement>();
-  // useEffect(() => {
-  //   const swipwer = new Swiper(swiperRef.current, {
-  //     loop: true,
-  //     slidesPerView: 3,
-  //     spaceBetween: 30,
-  //     slidesPerGroup: 1,
-  //     autoplay: {
-  //       delay: 2000,
-  //       disableOnInteraction: false,
-  //     },
-  //     breakpoints: {
-  //       992: {
-  //         slidesPerView: 1,
-  //         spaceBetween: 60,
-  //       },
-  //     },
-  //     navigation: {
-  //       nextEl: ".swiper-button-next",
-  //       prevEl: ".swiper-button-prev",
-  //     },
-  //   });
-  // }, []);
   return (
     <section className="testimonials">
       <header className="testimonials__header">
@@ -46,57 +23,36 @@ const Testimonials = () => {
             spaceBetween={30}
             slidesPerGroup={1}
             loop={true}
+            navigation={{
+              nextEl: ".next",
+              prevEl: ".prev",
+            }}
             // loopFillGroupWithBlank={true}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-            breakpoints={{
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-              1200: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-            }}
-            modules={[]}
+            // autoplay={{
+            //   delay: 1000,
+            //   disableOnInteraction: true,
+            // }}
+            // breakpoints={{
+            //   1024: {
+            //     slidesPerView: 3,
+            //     spaceBetween: 30,
+            //   },
+            //   1200: {
+            //     slidesPerView: 3,
+            //     spaceBetween: 30,
+            //   },
+            // }}
+            modules={[Autoplay, Navigation]}
           >
             <SwiperSlide>Slide 1</SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
             <SwiperSlide>Slide 3</SwiperSlide>
             <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            {/* <div className="swiper-wrapper"> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card"> */}
-            {/*       <Card /> */}
-            {/*     </div> */}
-            {/*   </div> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card">Card 2</div> */}
-            {/*   </div> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card">Card 3</div> */}
-            {/*   </div> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card">Card 4</div> */}
-            {/*   </div> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card">Card 5</div> */}
-            {/*   </div> */}
-            {/*   <div className="swiper-slide"> */}
-            {/*     <div className="card">Card 6</div> */}
-            {/*   </div> */}
-            {/* </div> */}
-            {/* <div className="swiper-button-prev"></div> */}
-            {/* <div className="swiper-button-next"></div> */}
           </Swiper>
+        </div>
+        <div className="arrow-buttons">
+          <button className="prev">{"<"}</button>
+          <button className="next">{">"}</button>
         </div>
       </header>
     </section>
